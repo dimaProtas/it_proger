3f8cb113bfb8b0247228014e43d307110f334fe1
dimaProtas <dima_protasevich92@mail.ru>
2024-04-21 19:41:41 +0300

    React состояния

--- src/App.js
+++ src/App.js
@@ -1,25 +1,41 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Header from "./companents/Header/Header";
+import lp from "./img/lp.jpg"
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      title: "Hello World",
+      userChange: '',
+    };
+    // Регестрация события для слвего метода, если пишешь в событии код то регестрировать его не нужно
+    this.onClick = this.onClick.bind(this);
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Header text='Home' />
+        <h1 onMouseEnter={this.mouseOverMain}>{this.state.title}</h1>
+        <h2>{this.state.userChange}</h2>
+        {/* // onChange прописали внутри события код и его регистрировать не нужно */}
+        <input onChange={event => this.setState({userChange: event.target.value})} onClick={this.onClick} placeholder="input" /> <br />
+        <img src={lp} alt="lp" />
+      </div>
+    );
+  }
+
+  mouseOverMain() {console.log("mouse over")}
+
+  onClick() {
+    this.setState({title: "Change text"})
+    console.log('Cliked')
+  }
+
 }
+
+
 
 export default App;
